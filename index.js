33ab7be639bdf8894fd4ce3faa5395a76b7e9917
Jeremy Evans <jeremy.n.evans@gmail.com>
2023-06-24 13:15:24 -0400

    Removed Chroma

--- index.js
+++ index.js
@@ -7,55 +7,10 @@ const get = require("lodash/get");
 const lancedb = require("vectordb");
 const { Configuration, OpenAIApi } = require("openai");
 const readline = require("readline/promises");
-const { Chroma } = require("langchain/vectorstores/chroma");
 const { OpenAIEmbeddings } = require("langchain/embeddings/openai");
-const { TextLoader } = require("langchain/document_loaders/fs/text");
 
 require("dotenv").config();
 const { stdin: input, stdout: output } = require("process");
-
-// load();
-async function load() {
-  // // Create docs with a loader
-  // const loader = new TextLoader("example.txt");
-  // const docs = await loader.load();
-
-  // text sample from Godel, Escher, Bach
-  const vectorStore = await Chroma.fromTexts(
-    [
-      `Tortoise: Labyrinth? Labyrinth? Could it Are we in the notorious Little
-        Harmonic Labyrinth of the dreaded Majotaur?`,
-      "Achilles: Yiikes! What is that?",
-      `Tortoise: They say-although I person never believed it myself-that an I
-        Majotaur has created a tiny labyrinth sits in a pit in the middle of
-        it, waiting innocent victims to get lost in its fears complexity.
-        Then, when they wander and dazed into the center, he laughs and
-        laughs at them-so hard, that he laughs them to death!`,
-      "Achilles: Oh, no!",
-      "Tortoise: But it's only a myth. Courage, Achilles.",
-    ],
-    [{ id: 2 }, { id: 1 }, { id: 3 }],
-    new OpenAIEmbeddings({
-      openAIApiKey: "",
-    }),
-    {
-      collectionName: "godel-escher-bach",
-    }
-  );
-
-  const response = await vectorStore.similaritySearch("scared", 2);
-
-  console.log(response);
-  /*
-[
-  Document { pageContent: 'Achilles: Oh, no!', metadata: {} },
-  Document {
-    pageContent: 'Achilles: Yiikes! What is that?',
-    metadata: { id: 1 }
-  }
-]
-*/
-}
 
 // Load the YAML file
 const filePath = "processes/get_manifesto_pledges.yml";
